ac1990c09d03693374831cf75e3bba8e56676a76
Craig M. Tonner <30607239+cmtonner@users.noreply.github.com>
2022-08-09 06:15:08 +0100

    Update AccountRepository.java
    Minor formatting corrections.

--- src/main/java/com/Natwest/repository/AccountRepository.java
+++ src/main/java/com/Natwest/repository/AccountRepository.java
@@ -13,13 +13,13 @@ public class AccountRepository {
 	private List<Account> accountList = new ArrayList<Account>();
 	
 	@Autowired
-    public void createAccounts() {
-    	accountList = List.of(
-    		new Account(11111, 111.11),
-            new Account(22222, 222.22),
-            new Account(33333, 333.33)
-        );
-    }
+    	public void createAccounts() {
+		accountList = List.of(
+		  new Account(11111, 111.11),
+		  new Account(22222, 222.22),
+		  new Account(33333, 333.33)
+		);
+    	}
 	
 	public List<Account> getAccounts() {
 		return accountList;
